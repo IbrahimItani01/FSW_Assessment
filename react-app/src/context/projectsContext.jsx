336126d9068a8c9442ec95ca2b5428f1866a7ba0
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-02 12:25:15 +0200

    js<>developed an async function to fetch projects data

--- react-app/src/context/projectsContext.jsx
+++ react-app/src/context/projectsContext.jsx
@@ -7,4 +7,18 @@ const getProjects = apiRoutes.getProjects;
 
   const [projects, setProjects] = useState([]);
 
+  const fetchProjects = async () => {
+    try {
+      const response = await fetch(getProjects);
+
+      if (!response.ok) {
+        throw new Error(`Error fetching projects: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      setProjects(data.projects); 
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 export default ProjectsProvider;
