75fabc9b1f017e8a01d3ecdb43086b8a283d16f4
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-02 12:24:03 +0200

    js<>imported necessary hooks from react

--- react-app/src/context/projectsContext.jsx
+++ react-app/src/context/projectsContext.jsx
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
+import apiRoutes from "../utils/api";
 
 export const projectsContext = createContext();
 
