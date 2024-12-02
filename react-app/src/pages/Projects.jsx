f3d44d377b2045b214db2a3a383886346604cb59
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-02 12:26:39 +0200

    js<>destructured the projects from the projectsContext using useContext hook

--- react-app/src/pages/Projects.jsx
+++ react-app/src/pages/Projects.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import Project from "../components/Project";
+import { projectsContext } from "../context/projectsContext";
 
 const Projects = () => {
+  const { projects } = useContext(projectsContext); // Access projects from the context
+
   return (
     <div className="projects-container">
       {projects.map((p) => (
