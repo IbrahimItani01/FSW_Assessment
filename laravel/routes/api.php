b27bc04fb31d4b4c53e6fb0aae2460a58e023742
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-02 12:11:59 +0200

    php<>imported all controllers

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\IncrementRequests;
+use App\Http\Controllers\Users;
+use App\Http\Controllers\Projects;
+use App\Http\Controllers\Members;
+
 Route::middleware(IncrementRequests::class)->group(function () {
 
     Route::prefix('users')->group(function () {
