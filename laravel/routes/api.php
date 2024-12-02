5e575b6b6eadfb5037416da06dc60adb07e4e582
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-02 11:40:56 +0200

    php<>created a middleware route

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\IncrementRequests;
+Route::middleware(IncrementRequests::class)->group(function () {
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+});
