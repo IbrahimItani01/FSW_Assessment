391033ac1f7326cd313904731fd83d42788d6b3d
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-02 12:12:31 +0200

    php<>implemented all operations of users group using all http request methods

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -10,7 +10,11 @@ use App\Http\Controllers\Members;
 Route::middleware(IncrementRequests::class)->group(function () {
 
     Route::prefix('users')->group(function () {
-        
+        Route::get('/', [Users::class, 'getAll']); 
+        Route::get('/{id}', [Users::class, 'getData']);
+        Route::post('/', [Users::class, 'create']); 
+        Route::put('/', [Users::class, 'update']); 
+        Route::delete('/{id}', [Users::class, 'delete']); 
     });
 
     Route::prefix('projects')->group(function () {
