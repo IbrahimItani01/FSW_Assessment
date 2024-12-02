1d2503108b75a31ad2864096b5182adbe5d72aa2
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-02 12:07:44 +0200

    php<>created an add method in members controller

--- laravel/app/Http/Controllers/Members.php
+++ laravel/app/Http/Controllers/Members.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Member;
 
 class Members extends Controller
 {
-    //
+    public function add(Request $request)
+    {
+        $validated = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'project_id' => 'required|exists:projects,id',
+        ]);
+
+        $member = Member::create([
+            'user_id' => $validated['user_id'],
+            'project_id' => $validated['project_id'],
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Member added to project successfully',
+            'member' => $member
+        ], 201);
+    }
+
 }
