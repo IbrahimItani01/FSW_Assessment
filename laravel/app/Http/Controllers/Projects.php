71ce13b3ce819ef4bb4138aad1d96d7d2c64a815
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-02 12:08:27 +0200

    php<>created a getAll data in projects controller

--- laravel/app/Http/Controllers/Projects.php
+++ laravel/app/Http/Controllers/Projects.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class Projects extends Controller
 {
-    //
+    public function getAll()
+    {
+        $projects = Project::all();
+        
+        if ($projects->isEmpty()) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'No projects found'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'projects' => $projects
+        ], 200);
+    }
 }
