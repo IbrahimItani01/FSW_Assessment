06c4fc2ba7808ba9a9e368a29bc389272ea99672
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-02 12:10:36 +0200

    php<>created a getAll method   in users controller

--- laravel/app/Http/Controllers/Users.php
+++ laravel/app/Http/Controllers/Users.php
@@ -8,5 +8,19 @@ use Illuminate\Support\Facades\Hash;
 
 class Users extends Controller
 {
-    //
+    public function getAll()
+    {
+        $users = User::all();
+        if ($users->isNotEmpty()) {
+            return response()->json([
+                'status' => 'success',
+                'users' => $users
+            ], 200);
+        }
+        return response()->json([
+            'status' => 'failed',
+            'message' => "Not found"
+        ], 404);
+    }
+
 }
